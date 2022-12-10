db541a5310533d49262b62aa3e845798c0e4a215
Drew Tingen <drew.tingen@icdsystems.com>
2022-12-10 08:44:03 -0500

    Finished 2022-12-10 Part 1

---
+++ AdventOfCode/Years/Year2022/Days/Day10/Input.cs
@@ -0,0 +1,168 @@
+﻿namespace AdventOfCode.Years.Year2022.Days.Day10;
+
+public static class Input
+{
+    public static IEnumerable<string> InputString
+    {
+        //get { return GetTestStrings(); }
+        //get { return GetTestStringLineByLine(); }
+        get { return System.IO.File.ReadLines(@"C:\Users\astin\Downloads\AoC\12-10-input.txt"); }
+    }
+
+    private static IEnumerable<string> GetTestStrings()
+    {
+        yield return @"";
+    }
+
+    private static IEnumerable<string> GetTestStringLineByLine()
+    {
+        return TEST_INPUT.Split(new string[] { "\r\n", "\r", "\n" }, StringSplitOptions.None);
+    }
+
+    private const string TEST_INPUT = @"addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop";
+}
---
+++ AdventOfCode/Years/Year2022/Days/Day10/Main.cs
@@ -0,0 +1,56 @@
+﻿namespace AdventOfCode.Years.Year2022.Days.Day10;
+
+public sealed class Main
+{
+    private static readonly int[] s_CheckCycles = new int[] {20, 60, 100, 140, 180, 220 };
+
+    public static void Run()
+    {
+        var checkedCycles = new SortedDictionary<int, bool>();
+        foreach (int i in s_CheckCycles)
+            checkedCycles.Add(i, false);
+
+        int currentCycle = 0;
+        int register = 1;
+        int signalStrenght = 0;
+
+        foreach (var instruction in Input.InputString)
+        {
+            int registerIncrement = 0;
+            // Check instruction, increase cycle appropriately
+            if (instruction.StartsWith("noop"))
+                currentCycle++;
+            else if (instruction.StartsWith("addx"))
+            {
+                currentCycle += 2;
+                registerIncrement = Int32.Parse(instruction.Substring(5));
+            }
+            else
+                throw new InvalidOperationException();
+            
+            // Check to see if we need to do anything here, before incrementing register
+            int nextCheckCycle;
+            try
+            {
+                nextCheckCycle = checkedCycles.First(kvp => !kvp.Value).Key;
+            }
+            catch (InvalidOperationException)
+            {
+                Console.WriteLine("Ran out of check cycles");
+                break;
+            }
+            
+            if (currentCycle >= nextCheckCycle)
+            {
+                //increase signal strength
+                signalStrenght += register * nextCheckCycle;
+                checkedCycles[nextCheckCycle] = true;
+            }
+            
+            //Increment register and continue
+            register += registerIncrement;
+        }
+        
+        Console.WriteLine($"Signal Strenngth Sum: {signalStrenght}");
+    }
+}
